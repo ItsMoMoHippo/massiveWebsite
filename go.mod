9f29a253fa48fc202dec4c48b14585a47f487bc2
Momo <pr0h3ckr@gmail.com>
2025-05-08 23:56:17 +0100

    switching over to templ

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module maWeb
 go 1.24.2
 
 require (
-	github.com/a-h/templ v0.3.857 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/a-h/templ v0.3.857
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 )
--- main.go
+++ main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
-	"maWeb/handler"
+	"maWeb/htmlstuff"
 	"net/http"
 
-	_ "github.com/a-h/templ"
+	"github.com/a-h/templ"
 	_ "github.com/lib/pq"
 )
 
 // main function
 func main() {
-	http.HandleFunc("/", handler.HomePage)
-	http.HandleFunc("/wharswap", handler.WharPage)
-	http.HandleFunc("/setClubFoo", handler.SetClubFoo)
-	http.HandleFunc("/changeTimes", handler.DifferentList)
+	homePage := htmlstuff.Page("MassiveAttackWeb", "MassiveAttack", "hello from templ")
+
+	http.Handle("/", templ.Handler(homePage))
+	http.Handle("/msgswap", templ.Handler(htmlstuff.Message("hello?")))
 
 	fmt.Println("Starting sevrer on :8080...")
 	http.ListenAndServe(":8080", nil)
